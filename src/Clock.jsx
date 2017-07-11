2880ef575475885f7b66dd82e064a65164e570b9
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-07-11 17:18:16 +0300

    Added helper leadingZero method

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -21,6 +21,10 @@ class Clock extends Component {
 		setInterval(() => this.getTimeUntil(this.props.deadline), 1000)
 	}
 
+	leadingZero(num) {
+		return num < 10 ? '0' + num : num
+	}
+
 	getTimeUntil(deadline) {
 		const time = Date.parse(deadline) - Date.parse(new Date())
 
@@ -44,10 +48,10 @@ class Clock extends Component {
 	render() {
 		return(
 			<div>
-				<div className="Clock-days">{this.state.days} days</div>
-				<div className="Clock-hours">{this.state.hours} hours</div>
-				<div className="Clock-minutes">{this.state.minutes} minutes</div>
-				<div className="Clock-seconds">{this.state.seconds} seconds</div>
+				<div className="Clock-days">{this.leadingZero(this.state.days)} days</div>
+				<div className="Clock-hours">{this.leadingZero(this.state.hours)} hours</div>
+				<div className="Clock-minutes">{this.leadingZero(this.state.minutes)} minutes</div>
+				<div className="Clock-seconds">{this.leadingZero(this.state.seconds)} seconds</div>
 			</div>
 		)
 	}
